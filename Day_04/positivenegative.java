cdd9ae12f39d69dffb1e389390577b1aa50a3a6b
Siddharth Gautam <siddharthgau9145@gmail.com>
2026-08-20 11:38:23 +0530

    Day_04 class

---
+++ Day_04/positivenegative.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class positivenegative{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if(num > 0){
+            System.out.println("Positvie Number");
+        }
+        else if(num < 0){
+            System.out.println("Negative Number");
+        }
+        else{
+            System.out.println("It's Zero");
+        }
+        sc.close();
+    }
+}
